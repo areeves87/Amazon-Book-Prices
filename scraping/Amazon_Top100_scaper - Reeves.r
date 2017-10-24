8a10460a14acb2aae61b6f0fac897e17108b3555
areeves87 <lxndr.rvs@gmail.com>
2017-10-24 10:39:46 -0700

    Improved comments

--- scraping/Amazon_Top100_scaper - Reeves.r
+++ scraping/Amazon_Top100_scaper - Reeves.r
@@ -8,12 +8,16 @@ library(data.table)
 ################### SCRAPE TOP BOOKS ###################
 #### source: amazon
 
+###Store URL for the page with all the categories listed in the sidebar.
 URL <- paste("https://www.amazon.com/best-sellers-books-Amazon/zgbs/books/ref=zg_bs_unv_b_1_4_1")
 
+###Scrape the html on that page, retrieve and store all links from html content, 
+###and then manually locate the indices for the links to the book categories.
 best_sellers_overall<- read_html(URL)
 links<-html_attr(html_nodes(best_sellers_overall, "a"), "href")
 best_seller_cats<-links[114:150]
-length(best_seller_cats)
+
+length(best_seller_cats) #Manually check to make sure you've grabbed all the listed book categories
 
 ####
 all_the_books<- data.frame()
@@ -21,14 +25,17 @@ all_the_books<- data.frame()
 #build a dataframe containing category,rank,title,price info 
 #for top 100 books per category
 
-for(i in 1:length(best_seller_cats)){
+#Can't simply scrape html content past page 1 of each category, need different strategy.
+#Find "hidden" API that is dynamically loading the book data for pages 2,3,4,5. See help file.
+
+for(i in 1:length(best_seller_cats)){    #loop through categories
         
         cat_base_url<-strsplit(best_seller_cats[i],"_nav")[[1]][1]
         
         cat_name_extract<-strsplit(best_seller_cats[i],"[-/]")      
         cat_name<-paste(cat_name_extract[[1]][7:(which(cat_name_extract[[1]]=="zgbs")-1)],
                         sep=" ",collapse=" ")
-        for(j in 1:5){
+        for(j in 1:5){   #loop through each category's pages
                 next_page<-paste0(cat_base_url,"_pg_",j,"?_encoding=UTF8&pg=",j,"&ajax=1")
                 page_data<- read_html(next_page)
                 
@@ -41,7 +48,7 @@ for(i in 1:length(best_seller_cats)){
                 page_summary<-cbind.data.frame(category,rank,titles,prices)
                 all_the_books<-rbind.data.frame(all_the_books,page_summary)
                 
-                Sys.sleep(2)
+                Sys.sleep(2) #be nice to the server so you don't get banned
         }
 }
 
